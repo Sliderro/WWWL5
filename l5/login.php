245a46513d56667b2417c09e9bff4e6ec62c165e
sliderro <arek.debski2@gmail.com>
2020-01-08 16:08:01 +0100

    initial commit

---
+++ l5/login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+  $username = $_POST["username"];
+  $password = hash("sha256", $_POST["password"]);
+
+  $db = new SQLite3("database.db");
+
+  $stmt = $db->prepare("SELECT password FROM USERS WHERE username=:usr");
+  $stmt->bindValue(":usr", $username, SQLITE3_TEXT);
+
+  $returned_set = $stmt->execute();
+
+  while($result = $returned_set->fetchArray(SQLITE3_ASSOC)) {
+    if ($result["password"] == $password) {
+      $_SESSION["username"] = $username;
+      setcookie(session_name(), session_id(), time() + 5 * 60);
+      header("Location: /index.php");
+    }
+  }
+  echo "login error.";
+}
+?>
+
+<form method="post" action="login.php">
+  <div class="input-group">
+    <label>Username</label>
+    <input type="text" name="username">
+  </div>
+  <div class="input-group">
+    <label>Password</label>
+    <input type="password" name="password">
+  </div>
+  <div class="input-group">
+    <button type="submit" class="btn">Login</button>
+  </div>
+  <p>
+    Does not have account? <a href="registration.php">Sign up</a>
+  </p>
+</form>
---
+++ l5/registration.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+  $username = $_POST["username"];
+  $password = hash("sha256", $_POST["password"]);
+  $email = $_POST["email"];
+
+  $db = new SQLite3("database.db");
+
+  $stmt = $db->prepare("INSERT INTO USERS VALUES (:usr, :psw, :eml)");
+  $stmt->bindValue(":usr", $username, SQLITE3_TEXT);
+  $stmt->bindValue(":psw", $password, SQLITE3_TEXT);
+  $stmt->bindValue(":eml", $email, SQLITE3_TEXT);
+  
+  if ($stmt->execute()) {
+    $_SESSION["username"] = $username;
+    setcookie(session_name(), session_id(), time() + 5 * 60);
+    header("Location: /index.php");
+  }
+  echo "registration error.";
+}
+?>
+
+<form method="post" action="registration.php">
+  <div class="input-group">
+    <label>Username</label>
+    <input type="text" name="username">
+  </div>
+  <div class="input-group">
+    <label>Password</label>
+    <input type="password" name="password">
+  </div>
+  <div class="input-group">
+    <label>E-mail</label>
+    <input type="email" name="email">
+  </div>
+  <div class="input-group">
+    <button type="submit" class="btn">Register</button>
+  </div>
+  <p>
+    Already a member? <a href="login.php">Sign in</a>
+  </p>
+</form>
